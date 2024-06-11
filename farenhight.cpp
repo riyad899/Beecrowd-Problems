2240e26e74ee65061bfed9eeebf9b0c17c7beb60
Riyadus Salehin <166088594+riyad-x@users.noreply.github.com>
2024-06-11 23:39:08 +0600

    Add files via upload

---
+++ farenhight.cpp
@@ -0,0 +1,32 @@
+
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+int main() 
+
+{
+    // Write C++ code here
+   int x = 3;
+  
+   x+=2; //x=x+y
+   
+    std::cout <<x  <<'\n';
+    x-=2;
+
+    std::cout << x <<'\n';
+
+    x*=2 ;
+    std::cout << x;
+    
+    
+    
+
+    
+}
+
+
+
+
+
+
+
